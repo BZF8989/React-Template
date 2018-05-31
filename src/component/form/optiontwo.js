9d2f6e5da78af5b4a8df4adde351bf3847c62f4f
Bill F <billfan10@gmail.com>
2018-05-30 22:42:36 -0700

    forms 2 updated, not finished

--- src/component/form/optiontwo.js
+++ src/component/form/optiontwo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Row, Col, Slider, Divider, InputNumber } from 'antd';
 
 
 
@@ -9,14 +9,68 @@ export class OptionTwo extends Component {
         super(props);
 
         this.state = {
-            
+            inputValueMax: 50,
+            inputValueMin: 20,
         }
+    }
+
+    onChange = (value) => {
+        console.log(value);
+        this.setState({
+            inputValueMax: value[1],
+            inputValueMin: value[0],
+        })
+    }
+
+    updateMax = (value) => {
+        console.log(value);
+        this.setState({
+            inputValueMax: value
+        })
+    }
+
+    updateMin = (value) => {
+        console.log(value);
+        this.setState({
+            inputValueMin: value
+        })
     }
 
     render() {
         return (
             <div>
-                This is Forms 2! :)
+                <h1>Special Forms!</h1>
+                <Row>
+                    <Col>
+                        <Divider/>
+                        <h2>How much are you willing to pay for a meal? </h2>
+                        <InputNumber
+                            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                            min={0}
+                            max={this.state.inputValueMax}
+                            style={{ marginLeft: 16 }}
+                            value={this.state.inputValueMin}
+                            onChange={this.updateMin}
+                            step={1}
+                        /> to
+                        <InputNumber
+                            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                            min={this.state.inputValueMin}
+                            max={100}
+                            style={{ marginLeft: 16 }}
+                            value={this.state.inputValueMax}
+                            onChange={this.updateMax}
+                            step={1}
+                        />
+                        <Slider
+                            range
+                            value={[this.state.inputValueMin, this.state.inputValueMax]}
+                            onChange={this.onChange}
+                        />
+                    </Col>
+                </Row>
             </div>
         );
     }
